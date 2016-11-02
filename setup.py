197e033d3e6fdf12cae8005e62dd773813e542af
Guillermo Hernández <guillehg@gmail.com>
2016-11-02 10:27:48 +0100

    Updated version number.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.1',
+    version='1.0.2',
 
     description='A package with a GUI to calculate X-ray sPECtra GENerated in tungsten anodes',
     long_description=long_description,
@@ -64,9 +64,9 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-	'Programming Language :: Python :: 2',
-	'Programming Language :: Python :: 2.6',
-	'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
